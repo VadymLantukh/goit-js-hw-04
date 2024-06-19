84a0e541467019b98fe6df3d7eec3c19b5a873cd
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-06-19 22:30:55 +0300

    Add HW-4 two task

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function isEnoughCapacity(products, containerSize) {
+  let totalSum = 0;
+  const valuesProducts = Object.values(products);
+
+  for (let element of valuesProducts) {
+    totalSum += element;
+  }
+
+  return totalSum <= containerSize ? true : false;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
